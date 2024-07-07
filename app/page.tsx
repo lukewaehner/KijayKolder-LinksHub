b66723e8490b29dded31a73efb4e9a5d79d8fb86
Luke Waehner <luke.waehner@gmail.com>
2024-07-07 18:42:43 -0400

    First push

---
+++ app/page.tsx
@@ -0,0 +1,115 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { Avatar } from "@nextui-org/avatar";
+import { Card, CardBody } from "@nextui-org/card";
+import { Link } from "@nextui-org/link";
+import { FaSoundcloud, FaApple, FaSpotify } from "react-icons/fa";
+
+import BackgroundMusic from "@/components/BackgroundMusic";
+const tracks = ["/audio/STAY.m4a", "/audio/ALONE-IN-DA-MUD.m4a"];
+const links = [
+  {
+    href: "https://google.com.com",
+    text: "Soundcloud",
+    icon: <FaSoundcloud className="text-2xl" style={{ color: "#ff5500" }} />,
+  },
+  {
+    href: "https://google.com.com",
+    text: "Apple Music",
+    icon: <FaApple className="text-2xl" />,
+  },
+  {
+    href: "https://google.com.com",
+    text: "Spotify",
+    icon: <FaSpotify className="text-2xl" style={{ color: "#1db954" }} />,
+  },
+];
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      {/* Main Content Container */}
+      <div className="relative p-8 py-12 backdrop-blur-[8px] rounded-xl border border-gray-300/[0.15] shadow-md w-full max-w-md z-10">
+        {/* Content */}
+        <div className="flex flex-col items-center justify-center gap-6">
+          {/* Animated Card */}
+          <motion.div
+            initial={{ opacity: 0, scale: 0.9 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.6, ease: "easeOut" }}
+          >
+            <Card className="px-4 rounded-lg shadow-lg border-4 border-gray-50/[0.2]">
+              <CardBody>
+                <h1 className="text-2xl text-zinc-800 font-semibold">
+                  KiJayKolder
+                </h1>
+              </CardBody>
+            </Card>
+          </motion.div>
+
+          {/* Animated Avatar */}
+          <motion.div
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.6, delay: 0.3, ease: "easeOut" }}
+          >
+            <Avatar
+              src="/images/avatar.png"
+              name="KiJay"
+              isBordered
+              className="w-32 h-32 text-large filter drop-shadow-2xl"
+            />
+          </motion.div>
+
+          {/* Animated Background Music Component */}
+          <motion.div
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5, delay: 1, ease: "easeOut" }}
+          >
+            <BackgroundMusic tracks={tracks} />
+          </motion.div>
+
+          {/* Animated Links Container */}
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.6, delay: 0.8, ease: "easeOut" }}
+            className="flex flex-col gap-4"
+          >
+            {/* Flex column layout for links */}
+            {links.map((link, index) => (
+              <motion.div
+                key={index}
+                className="flex items-center gap-2"
+                initial={{ opacity: 0, x: -20 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{
+                  duration: 0.6,
+                  delay: 1 + index * 0.2,
+                  ease: "easeOut",
+                }}
+              >
+                {/* Flex row layout for icon and text */}
+                <Link href={link.href}>
+                  <Card className="rounded-full px-3 flex items-center gap-2">
+                    <CardBody className="min-w-44">
+                      <div className="flex items-center gap-2">
+                        {/* Flex row layout for icon and text */}
+                        {link.icon}
+                        <h2 className="text-xl text-black font-semibold">
+                          {link.text}
+                        </h2>
+                      </div>
+                    </CardBody>
+                  </Card>
+                </Link>
+              </motion.div>
+            ))}
+          </motion.div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ components/BackgroundMusic.tsx
@@ -0,0 +1,112 @@
+"use client";
+
+import { useState, useEffect, useRef } from "react";
+import {
+  MdPlayCircle,
+  MdPauseCircle,
+  MdSkipNext,
+  MdSkipPrevious,
+} from "react-icons/md";
+
+interface BackgroundMusicProps {
+  tracks: string[];
+  loop?: boolean;
+}
+
+const BackgroundMusic: React.FC<BackgroundMusicProps> = ({
+  tracks,
+  loop = true,
+}) => {
+  const audioRef = useRef<HTMLAudioElement>(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [currentTrackIndex, setCurrentTrackIndex] = useState(0);
+
+  const currentTrack = tracks[currentTrackIndex];
+
+  useEffect(() => {
+    if (audioRef.current) {
+      audioRef.current.src = currentTrack;
+
+      // Handle play and pause events to update the state
+      const handlePlay = () => setIsPlaying(true);
+      const handlePause = () => setIsPlaying(false);
+
+      audioRef.current.addEventListener("play", handlePlay);
+      audioRef.current.addEventListener("pause", handlePause);
+
+      // Clean up event listeners on component unmount
+      return () => {
+        if (audioRef.current) {
+          audioRef.current.removeEventListener("play", handlePlay);
+          audioRef.current.removeEventListener("pause", handlePause);
+        }
+      };
+    }
+  }, [currentTrack]);
+
+  useEffect(() => {
+    if (audioRef.current) {
+      // Play or pause the audio based on the isPlaying state
+      if (isPlaying) {
+        audioRef.current
+          .play()
+          .catch((error) => console.error("Audio playback failed:", error));
+      } else {
+        audioRef.current.pause();
+      }
+    }
+  }, [isPlaying]);
+
+  useEffect(() => {
+    if (audioRef.current) {
+      // Automatically play the new track when the currentTrack changes
+      audioRef.current
+        .play()
+        .catch((error) => console.error("Audio playback failed:", error));
+    }
+  }, [currentTrack]);
+
+  const togglePlay = () => {
+    setIsPlaying(!isPlaying);
+  };
+
+  const skipToNextTrack = () => {
+    setCurrentTrackIndex((prevIndex) => (prevIndex + 1) % tracks.length);
+  };
+
+  const rewindToStart = () => {
+    if (audioRef.current) {
+      audioRef.current.currentTime = 0;
+    }
+  };
+
+  return (
+    <div className="flex items-center gap-4">
+      <button
+        onClick={rewindToStart}
+        className="bg-white hover:bg-gray-300 text-black rounded-full p-2 focus:outline-none"
+        aria-label="Rewind"
+      >
+        <MdSkipPrevious size={24} />
+      </button>
+
+      <button
+        onClick={togglePlay}
+        className="bg-white hover:bg-gray-300 text-black rounded-full p-2 focus:outline-none"
+        aria-label={isPlaying ? "Pause Music" : "Play Music"}
+      >
+        {isPlaying ? <MdPauseCircle size={24} /> : <MdPlayCircle size={24} />}
+      </button>
+      <button
+        onClick={skipToNextTrack}
+        className="bg-white hover:bg-gray-300 text-black rounded-full p-2 focus:outline-none"
+        aria-label="Skip to Next Track"
+      >
+        <MdSkipNext size={24} />
+      </button>
+      <audio ref={audioRef} loop={loop} />
+    </div>
+  );
+};
+
+export default BackgroundMusic;
